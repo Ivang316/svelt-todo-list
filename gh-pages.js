e6e45bee91338de44c68cc83f1cded168aab3597
Artem <artemtolv@gmail.com>
2021-07-17 16:15:47 +0300

    Create totlist

---
+++ gh-pages.js
@@ -0,0 +1,10 @@
+const { publish } = require('gh-pages');
+
+publish(
+    'public',
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/Ivang316/svelte-todolist.git',
+    },
+    () => console.log('Deploy Complete!')
+);
---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { v4 as uuid } from 'uuid';
+	let todos = [];
+	let newTodoText = '';
+
+	const addTodo = () => {
+		todos.unshift({ id: uuid(), text: newTodoText });
+		newTodoText = '';
+	};
+	const deleteTodo = (id) => todos = todos.filter((t) => t.id !== id);
+</script>
+
+<main>
+	<h2>Todo App</h2>
+	<div>
+		<input bind:value={newTodoText}/>
+		<button on:click={addTodo}>Add</button>
+	</div>
+	{#each todos as t (t.id)}
+		<div>
+			{t.text}
+			<button on:click={deleteTodo(t.id)}>Delete</button>
+		</div>
+	{/each}
+</main>
